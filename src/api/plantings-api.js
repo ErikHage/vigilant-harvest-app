212f79629ce27539b8f244e61af2a480dec1898c
Erik Hage <ehage4@gmail.com>
2024-07-18 17:47:04 -0400

    add plantings api

---
+++ src/api/plantings-api.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+import { vigilantHarvestServiceUrl } from "@/utils/constants";
+
+async function upsertPlanting(actorToken, plant) {
+    try {
+        const response = await axios.put(`${vigilantHarvestServiceUrl.v0.api}/plantings`, plant, {
+            headers: {
+                'x-feral-auth-token': actorToken,
+            },
+        });
+        return response.data;
+    } catch (err) {
+        console.log('error upserting planting');
+        throw err;
+    }
+}
+
+async function fetchPlantingsByYear(actorToken, plantingYear) {
+    try {
+        const response = await axios.get(`${vigilantHarvestServiceUrl.v0.api}/plantings`, {
+            params: {
+                plantingYear,
+            },
+            headers: {
+                'x-feral-auth-token': actorToken,
+            },
+        });
+        return response.data;
+    } catch (err) {
+        console.log('error fetching plantings');
+        throw err;
+    }
+}
+
+export default {
+    upsertPlanting,
+    fetchPlantingsByYear,
+};
