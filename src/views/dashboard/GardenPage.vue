e455b654eec4717fb5ba4391bb90904c8eba441e
Erik Hage <ehage4@gmail.com>
2024-08-01 01:17:30 -0400

    select year on garden overview page

--- src/views/dashboard/GardenPage.vue
+++ src/views/dashboard/GardenPage.vue
@@ -2,7 +2,24 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-        <v-card>
+        <v-card v-if="selectedYear == null">
+          <v-card-title>
+            <v-spacer></v-spacer>
+            <v-select
+                v-model="selectedYear"
+                :items="availableYears"
+                :item-title="(year) => year"
+                :item-value="(year) => year"
+                label="Select Year"
+                variant="solo"
+                @update:model-value="onSelectYearChange"
+            ></v-select>
+          </v-card-title>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12">
+        <v-card v-if="selectedYear != null">
           <v-card-title>
             <span class="headline">Garden</span>
           </v-card-title>
@@ -18,9 +35,29 @@
 
 <script>
 
+import { mapActions } from "pinia";
+import { usePlantingsStore } from "@/store";
+
 export default {
   name: 'GardenPage',
 
   components: {},
+
+  data: () => ({
+    availableYears: [2024, 2025], // TODO source from backend
+    selectedYear: null,
+  }),
+
+  methods: {
+    async onSelectYearChange(year) {
+
+    },
+
+    async refreshData() {
+      if (this.selectedYear) {
+        await this.fetchPlantingsByYear(this.selectedYear);
+      }
+    },
+  },
 }
 </script>
