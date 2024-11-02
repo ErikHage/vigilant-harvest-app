5d00a7e1659a25719a3b950e2c7149fd7d02eea2
Erik Hage <ehage4@gmail.com>
2024-11-01 23:14:27 -0400

    use constants for paths

--- src/components/layout/DashboardAppBar.vue
+++ src/components/layout/DashboardAppBar.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar app>
-    <v-btn v-if="showYearSpecificViews" to="/dashboard/garden">Garden</v-btn>
-    <v-btn to="/dashboard/plants">Plants</v-btn>
-    <v-btn to="/dashboard/plots">Plots</v-btn>
-    <v-btn v-if="showYearSpecificViews" to="/dashboard/plantings">Plantings</v-btn>
-    <v-btn v-if="showYearSpecificViews" to="/dashboard/harvests">Harvests</v-btn>
+    <v-btn v-if="showYearSpecificViews" :to="gardenPagePath">Garden</v-btn>
+    <v-btn :to="plantsPagePath">Plants</v-btn>
+    <v-btn :to="plotsPagePath">Plots</v-btn>
+    <v-btn v-if="showYearSpecificViews" :to="plantingsPagePath">Plantings</v-btn>
+    <v-btn v-if="showYearSpecificViews" :to="harvestsPagePath">Harvests</v-btn>
   </v-app-bar>
 </template>
 
@@ -18,6 +18,11 @@ export default {
 
   data: () => ({
     dashboardPath: views.dashboard.path,
+    gardenPagePath: this.dashboardPath + "/" + views.dashboard.children.garden.path,
+    plantsPagePath: this.dashboardPath + "/" + views.dashboard.children.plants.path,
+    plotsPagePath: this.dashboardPath + "/" + views.dashboard.children.plots.path,
+    plantingsPagePath: this.dashboardPath + "/" + views.dashboard.children.plantings.path,
+    harvestsPagePath: this.dashboardPath + "/" + views.dashboard.children.harvests.path,
   }),
 
   computed: {
