cfaa72ebd6307a4479edb0bddc8496dad38a0cd1
Erik Hage <ehage4@gmail.com>
2024-11-09 22:55:51 -0500

    harvests page styling

--- src/components/harvests/HarvestsList.vue
+++ src/components/harvests/HarvestsList.vue
@@ -1,12 +1,19 @@
 <template>
   <v-card class="harvest-card" v-for="harvestDate in harvestDates" >
-    <v-card-title class="">
+    <v-card-title>
       {{ formatHarvestDate(harvestDate) }}
     </v-card-title>
     <v-card-text>
-      <div v-for="harvest in hydratedHarvestsByDate[harvestDate]">
-        {{ harvest.quantity }} - {{ harvest.plantName }}
-      </div>
+      <v-table class="no-lines" density="compact">
+        <tbody>
+        <tr v-for="harvest in hydratedHarvestsByDate[harvestDate]">
+          <td class="equal-columns text-right">
+            <v-chip :color="getQuantityColor(harvest.quantity)">{{ harvest.quantity }}</v-chip>
+          </td>
+          <td class="equal-columns text-left">{{ harvest.plantName }}</td>
+        </tr>
+        </tbody>
+      </v-table>
     </v-card-text>
   </v-card>
 </template>
@@ -23,9 +30,23 @@
     methods: {
       formatHarvestDate(dateTimeString) {
         const date = new Date(dateTimeString);
-        const options = { month: 'short', day: 'numeric' };
+        const options = { month: 'long', day: 'numeric' };
         return date.toLocaleDateString('en-US', options);
       },
+
+      getQuantityColor(quantity) {
+        if (quantity <= 1) {
+          return "#4575b4";
+        } else if (quantity <= 5) {
+          return "#91bfdb"
+        } else if (quantity <= 10) {
+          return "#ffffbf"
+        } else if (quantity <= 20) {
+          return "#fdae61"
+        } else {
+          return "#d73027";
+        }
+      }
     },
   }
 </script>
@@ -33,5 +54,20 @@
 <style scoped>
   .harvest-card {
     margin-bottom: 5px;
+  }
+
+  .no-lines {
+    border: none;
+    border-collapse: collapse;
+  }
+
+  .no-lines tr,
+  .no-lines th,
+  .no-lines td {
+    border: none !important;
+  }
+
+  .equal-columns {
+    width: 50%;
   }
 </style>
