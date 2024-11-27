69233c5a90bdc5a298f7aaa78315893f333ece08
Erik Hage <ehage4@gmail.com>
2024-11-26 19:53:35 -0500

    add stats page skeleton

---
+++ src/components/views/dashboard/StatsPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container>
+
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <page-title title="Stats"/>
+        <v-spacer></v-spacer>
+      </v-col>
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+
+<!--        DATA HERE -->
+
+      </v-col>
+      <v-col cols="2"></v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+
+import {mapActions, mapState} from "pinia";
+import {usePlotsStore} from "@/store";
+import PageTitle from "@/components/layout/PageTitle.vue";
+import PlotsTable from "@/components/plots/PlotsTable.vue";
+import UpsertPlotDialog from "@/components/plots/UpsertPlotDialog.vue";
+
+export default {
+  name: 'StatsPage',
+
+  components: {
+    PageTitle,
+  },
+
+  data: () => ({
+  }),
+
+  methods: {
+    ...mapActions(usePlotsStore, [
+      'upsertPlot',
+      'fetchPlots',
+      'deletePlotById',
+    ]),
+
+    async refreshData() {
+    },
+  },
+
+  mounted() {
+    this.refreshData();
+  }
+}
+</script>
