8c19f4ee8004ccd5c051ca1ceede165c79566397
Erik Hage <ehage4@gmail.com>
2024-12-26 14:08:44 -0500

    add planting stats card component

---
+++ src/components/plantings/PlantingStatsCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <h3>{{ plantingStats.plantName }}</h3>
+      <span class="subtitle">{{ plantingStats.plantingId }}</span>
+    </v-card-title>
+    <v-card-text>
+      <v-table>
+        <tbody>
+        <tr>
+          <th>First Harvest</th>
+          <td>{{ this.formatDate(plantingStats.firstHarvest) }}</td>
+        </tr>
+        <tr>
+          <th>Last Harvest</th>
+          <td>{{ this.formatDate(plantingStats.lastHarvest) }}</td>
+        </tr>
+        <tr>
+          <th>Total Harvested</th>
+          <td>{{ plantingStats.totalQuantity }}</td>
+        </tr>
+        <tr>
+          <th>Average Harvest per Day</th>
+          <td>{{ plantingStats.averageHarvestPerDay }}</td>
+        </tr>
+        </tbody>
+      </v-table>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "PlantingStatsCard",
+
+  props: {
+    plantingStats: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    formatDate(date) {
+      return date ? new Date(date).toDateString() : '---';
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
