7d4afa0ae8adcbe3214a3d979d7b93fafa9cbfb4
Erik Hage <ehage4@gmail.com>
2025-01-10 17:35:46 -0500

    only enable update button when there are changes

--- src/components/views/dashboard/plants/PlantDetailsPage.vue
+++ src/components/views/dashboard/plants/PlantDetailsPage.vue
@@ -11,7 +11,8 @@
         <v-snackbar
             v-model="snackbar.show"
             timeout="2000"
-        >{{ snackbar.message }}</v-snackbar>
+        >{{ snackbar.message }}
+        </v-snackbar>
 
         <v-row v-if="loading" class="mt-10 d-flex justify-center">
           <v-progress-circular size="100" width="10" color="green" indeterminate/>
@@ -21,7 +22,29 @@
           <v-col cols="12" class="text-center">
             <page-title :title="plantCopy.friendlyName"/>
             <v-spacer/>
-            <v-btn size="small" color="blue darken-1" @click="updatePlant">Update</v-btn>
+            <v-btn
+                class="mr-4"
+                size="small"
+                color="blue darken-1"
+                :disabled="!updateButtonEnabled"
+                @click="updatePlant">Update
+            </v-btn>
+            <v-btn
+                size="small"
+                color="gray darken-1"
+                :disabled="!updateButtonEnabled"
+                @click="refreshData">Reset
+            </v-btn>
+          </v-col>
+
+          <v-col cols="12">
+            <v-card>
+              <v-card-text>
+                <v-text-field v-model="plantId" label="Id" variant="solo" density="compact" disabled/>
+                <v-text-field v-model="createdAt" label="Created At" variant="solo" density="compact" disabled/>
+                <v-text-field v-model="lastModifiedAt" label="Last Updated" variant="solo" density="compact" disabled/>
+              </v-card-text>
+            </v-card>
           </v-col>
         </v-row>
 
@@ -30,11 +53,8 @@
             <v-card>
               <v-card-title>Details</v-card-title>
               <v-card-text>
-                <v-text-field v-model="plantCopy.friendlyName" label="Name" variant="solo" density="compact" required />
-<!--                <v-text-field v-model="plantCopy.category" label="Category" variant="solo" density="compact" required></v-text-field> -->
-                <v-text-field v-model="plantId" label="Id" variant="solo" density="compact" disabled />
-                <v-text-field v-model="createdAt" label="Created At" variant="solo" density="compact" disabled />
-                <v-text-field v-model="lastModifiedAt" label="Last Updated" variant="solo" density="compact" disabled />
+                <v-text-field v-model="plantCopy.friendlyName" label="Name" variant="solo" density="compact" required/>
+                <!--                <v-text-field v-model="plantCopy.category" label="Category" variant="solo" density="compact" required></v-text-field> -->
               </v-card-text>
             </v-card>
           </v-col>
@@ -42,9 +62,9 @@
             <v-card>
               <v-card-title>Taxonomy</v-card-title>
               <v-card-text>
-                <v-text-field v-model="plantCopy.family" label="Family" variant="solo" density="compact" required />
-                <v-text-field v-model="plantCopy.genus" label="Genus" variant="solo" density="compact" required />
-                <v-text-field v-model="plantCopy.species" label="Species" variant="solo" density="compact" required />
+                <v-text-field v-model="plantCopy.family" label="Family" variant="solo" density="compact" required/>
+                <v-text-field v-model="plantCopy.genus" label="Genus" variant="solo" density="compact" required/>
+                <v-text-field v-model="plantCopy.species" label="Species" variant="solo" density="compact" required/>
               </v-card-text>
             </v-card>
           </v-col>
@@ -104,6 +124,13 @@ export default {
     lastModifiedAt() {
       return this.plantCopy ? new Date(this.plantCopy.dateModified).toLocaleString() : '--';
     },
+
+    updateButtonEnabled() {
+      return this.plant.friendlyName !== this.plantCopy.friendlyName?.trim()
+          || this.plant.family !== this.plantCopy.family?.trim()
+          || this.plant.genus !== this.plantCopy.genus?.trim()
+          || this.plant.species !== this.plantCopy.species?.trim();
+    }
   },
 
   methods: {
@@ -120,10 +147,10 @@ export default {
     async updatePlant() {
       await this.upsertPlant({
         plantId: this.plantCopy.plantId,
-        friendlyName: this.plantCopy.friendlyName,
-        family: this.plantCopy.family,
-        genus: this.plantCopy.genus,
-        species: this.plantCopy.species,
+        friendlyName: this.plantCopy.friendlyName?.trim(),
+        family: this.plantCopy.family?.trim(),
+        genus: this.plantCopy.genus?.trim(),
+        species: this.plantCopy.species?.trim(),
       });
       await this.refreshData();
 
