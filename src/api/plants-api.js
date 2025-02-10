cc8209b9f3438913a004432494de611a23feb039
Erik Hage <ehage4@gmail.com>
2025-02-10 16:49:46 -0500

    add error handler to plants api calls

--- src/api/plants-api.js
+++ src/api/plants-api.js
@@ -1,66 +1,47 @@
 import axios from "axios";
 
+import { withApiErrorHandling } from './error-handler';
 import { vigilantHarvestServiceUrl } from "@/utils/constants";
 
 async function upsertPlant(actorToken, plant) {
-    try {
-        const response = await axios.put(`${vigilantHarvestServiceUrl.v0}/plants`, plant, {
-            headers: {
-                'x-feral-auth-token': actorToken,
-            },
-        });
-        return response.data;
-    } catch (err) {
-        console.log('error upserting plant');
-        throw err;
-    }
+    const response = await axios.put(`${vigilantHarvestServiceUrl.v0}/plants`, plant, {
+        headers: {
+            'x-feral-auth-token': actorToken,
+        },
+    });
+    return response.data;
 }
 
 async function fetchPlants(actorToken) {
-    try {
-        const response = await axios.get(`${vigilantHarvestServiceUrl.v0}/plants`, {
-            headers: {
-                'x-feral-auth-token': actorToken,
-            },
-        });
-        return response.data;
-    } catch (err) {
-        console.log('error fetching plants');
-        throw err;
-    }
+    const response = await axios.get(`${vigilantHarvestServiceUrl.v0}/plants`, {
+        headers: {
+            'x-feral-auth-token': actorToken,
+        },
+    });
+    return response.data;
 }
 
 async function fetchPlantById(actorToken, plantId) {
-    try {
-        const response = await axios.get(`${vigilantHarvestServiceUrl.v0}/plants/${plantId}`, {
-            headers: {
-                'x-feral-auth-token': actorToken,
-            },
-        });
-        return response.data;
-    } catch (err) {
-        console.log('error fetching plant by id');
-        throw err;
-    }
+    const response = await axios.get(`${vigilantHarvestServiceUrl.v0}/plants/${plantId}`, {
+        headers: {
+            'x-feral-auth-token': actorToken,
+        },
+    });
+    return response.data;
 }
 
 async function deletePlantById(actorToken, plantId) {
-    try {
-        const response = await axios.delete(`${vigilantHarvestServiceUrl.v0}/plants/${plantId}`, {
-            headers: {
-                'x-feral-auth-token': actorToken,
-            },
-        });
-        return response.data;
-    } catch (err) {
-        console.log('error deleting plant by id');
-        throw err;
-    }
+    const response = await axios.delete(`${vigilantHarvestServiceUrl.v0}/plants/${plantId}`, {
+        headers: {
+            'x-feral-auth-token': actorToken,
+        },
+    });
+    return response.data;
 }
 
 export default {
-    upsertPlant,
-    fetchPlants,
-    fetchPlantById,
-    deletePlantById,
+    upsertPlant: withApiErrorHandling(upsertPlant),
+    fetchPlants: withApiErrorHandling(fetchPlants),
+    fetchPlantById: withApiErrorHandling(fetchPlantById),
+    deletePlantById: withApiErrorHandling(deletePlantById),
 };
