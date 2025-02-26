ea5e3670d9ffdf08c499d6091c751335c66a9f2c
Erik Hage <ehage4@gmail.com>
2025-02-26 17:47:11 -0500

    add PlanningPage skeleton

---
+++ src/components/views/dashboard/planning/PlanningPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="text-center">
+        <page-title title="Planning"/>
+        <v-spacer></v-spacer>
+        <fade-out-alert
+            v-for="(alert, i) in alerts"
+            :key="'alert' + i"
+            :is-visible="alert.isVisible"
+            :alert-type="alert.type"
+            :message="alert.message"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import FadeOutAlert from "@/components/utils/FadeOutAlert.vue";
+import PageTitle from "@/components/layout/PageTitle.vue";
+
+export default {
+  name: "PlanningPage",
+
+  components: {
+    PageTitle,
+    FadeOutAlert,
+  },
+
+  computed: {
+    alerts() {
+      return [];
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
