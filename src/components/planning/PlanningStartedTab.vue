c6ac3cbaea4a8e7c56e61eb25df3827d80ffaaea
Erik Hage <ehage4@gmail.com>
2025-02-26 18:19:33 -0500

    add created and started tabs to PlanningPage

---
+++ src/components/planning/PlanningStartedTab.vue
@@ -0,0 +1,24 @@
+<template>
+  {{ plantings }}
+</template>
+
+<script>
+export default {
+  name: "PlanningStartedTab",
+
+  props: {
+    plantings: Array,
+    plantsMap: Object,
+  },
+}
+</script>
+
+<style scoped>
+.shrink-column {
+  white-space: nowrap;
+  width: 1%;
+}
+.multiline-cell {
+  white-space: pre-wrap;
+}
+</style>
--- src/components/views/dashboard/planning/PlanningPage.vue
+++ src/components/views/dashboard/planning/PlanningPage.vue
@@ -12,26 +12,107 @@
             :message="alert.message"
         />
       </v-col>
+
+      <v-col cols="2"></v-col>
+      <v-col cols="8">
+
+        <v-card class="mt-4">
+          <v-tabs
+              v-model="tab"
+              align-tabs="start"
+              fixed-tabs>
+            <v-tab value="created">Created</v-tab>
+            <v-tab value="started">Started</v-tab>
+          </v-tabs>
+
+          <v-card-text>
+            <v-tabs-window v-model="tab">
+
+              <v-tabs-window-item value="created">
+                <planning-created-tab :plantings="createdPlantings" :plants-map="plantsById"/>
+              </v-tabs-window-item>
+
+              <v-tabs-window-item value="started">
+                <planning-started-tab :plantings="startedPlantings" :plants-map="plantsById"/>
+              </v-tabs-window-item>
+
+            </v-tabs-window>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="2"></v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { mapState } from "pinia";
+import { usePlantingsStore, usePlantsStore } from "@/store";
+import plantingUtils from '@/utils/plantings';
+
 import FadeOutAlert from "@/components/utils/FadeOutAlert.vue";
 import PageTitle from "@/components/layout/PageTitle.vue";
+import PlantingDetailsTab from "@/components/plantings/PlantingDetailsTab.vue";
+import PlantingHistoryTab from "@/components/plantings/PlantingHistoryTab.vue";
+import PlanningCreatedTab from "@/components/planning/PlanningCreatedTab.vue";
+import PlanningStartedTab from "@/components/planning/PlanningStartedTab.vue";
+
+const { plantingStatuses } = plantingUtils;
 
 export default {
   name: "PlanningPage",
 
   components: {
+    PlanningStartedTab,
+    PlanningCreatedTab,
+    PlantingHistoryTab, PlantingDetailsTab,
     PageTitle,
     FadeOutAlert,
   },
 
+  data() {
+    return {
+      tab: null,
+    };
+  },
+
   computed: {
+    ...mapState(usePlantsStore, {
+      plantsById: 'plantsById',
+      plantsAlertType: 'alertType',
+      plantsAlertMessage: 'alertMessage',
+      plantsAlertVisible: 'alertVisible',
+    }),
+
+    ...mapState(usePlantingsStore, {
+      plantings: 'plantings',
+      plantingsAlertType: 'alertType',
+      plantingsAlertMessage: 'alertMessage',
+      plantingsAlertVisible: 'alertVisible',
+    }),
+
+    createdPlantings() {
+      return this.plantings.filter(planting => planting.currentStatus === plantingStatuses.created);
+    },
+
+    startedPlantings() {
+      return this.plantings.filter(planting => planting.currentStatus === plantingStatuses.started);
+    },
+
     alerts() {
-      return [];
-    }
+      return [
+        {
+          isVisible: this.plantingsAlertVisible,
+          type: this.plantingsAlertType,
+          message: this.plantingsAlertMessage,
+        },
+        {
+          isVisible: this.plantsAlertVisible,
+          type: this.plantsAlertType,
+          message: this.plantsAlertMessage,
+        },
+      ];
+    },
   },
 }
 </script>
