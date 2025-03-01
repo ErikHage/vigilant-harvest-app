f76e62d0e5fa472732a7fe6d340f57fe63612a95
Erik Hage <ehage4@gmail.com>
2025-02-28 22:08:58 -0500

    extract heatmap to display utils

--- src/components/planning/PlanningCreatedTab.vue
+++ src/components/planning/PlanningCreatedTab.vue
@@ -34,18 +34,7 @@
 <script>
 import dayjs from 'dayjs';
 
-const progressColorMap = {
-  [0]: '#800000', // dark red
-  [1]: '#C00000', // red
-  [2]: '#FF0000', // bright red
-  [3]: '#FF4000', // dark orange
-  [4]: '#FF8000', // orange
-  [5]: '#FFFF00', // yellow
-  [6]: '#BFFF00', // yellow-green
-  [7]: '#80FF00', // light green
-  [8]: '#00C000', // green
-  [9]: '#006000', // dark green
-};
+import { heatMap } from '@/utils/display'
 
 export default {
   name: "PlanningCreatedTab",
@@ -102,7 +91,7 @@ export default {
     getProgressColor(planting) {
       const progress = this.getPlantingProgress(planting);
 
-      return progressColorMap[Math.floor(progress / 10)];
+      return heatMap[Math.floor(progress / 10)];
     },
 
     onClickCard(planting) {
