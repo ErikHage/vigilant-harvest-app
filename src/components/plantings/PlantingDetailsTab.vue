7996279d0e4fc51bbdcb5408bd7b018349c30540
Erik Hage <ehage4@gmail.com>
2025-03-02 00:02:09 -0500

    make some fields editable in planting details page (no update action yet)

--- src/components/plantings/PlantingDetailsTab.vue
+++ src/components/plantings/PlantingDetailsTab.vue
@@ -1,39 +1,120 @@
 <template>
-  <v-text-field v-model="planting.plantingId" label="Id" variant="solo" density="compact" disabled/>
+  <div class="d-flex justify-space-between">
+    <v-text-field
+        v-model="plantingCopy.plantingId"
+        label="Id"
+        variant="solo"
+        density="compact"
+        disabled/>
 
-  <div class="d-flex">
-    <v-text-field v-model="createdAt" label="Created At" variant="solo" density="compact" disabled/>
-    <v-text-field v-model="lastModifiedAt" label="Last Updated" variant="solo" density="compact" disabled/>
+    <v-switch
+        v-model="enableEdit"
+        color="#00C000"
+        base-color="#C00000">
+      <template v-slot:label>
+        <v-icon>{{ enableEdit ? 'mdi-lock-open' : 'mdi-lock' }}</v-icon>
+      </template>
+    </v-switch>
   </div>
 
   <div class="d-flex">
-    <v-text-field v-model="planting.seedSource" label="source" variant="solo" density="compact" disabled/>
-    <v-text-field v-model="planting.lotNumber" label="Lot#" variant="solo" density="compact" disabled/>
+    <v-text-field
+        v-model="createdAt"
+        label="Created At"
+        variant="solo"
+        density="compact"
+        disabled/>
+    <v-text-field
+        v-model="lastModifiedAt"
+        label="Last Updated"
+        variant="solo"
+        density="compact"
+        disabled/>
   </div>
 
   <div class="d-flex">
-    <v-btn class="px-1 mt-2" color="black" size="small" @click="navigateToPlantDetails(planting.plantId)">
+    <v-text-field
+        v-model="plantingCopy.seedSource"
+        label="source"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled()"/>
+    <v-text-field
+        v-model="plantingCopy.lotNumber"
+        label="Lot#"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled()"/>
+  </div>
+
+  <div class="d-flex">
+    <v-btn class="px-1 mt-2" color="black" size="small" @click="navigateToPlantDetails(plantingCopy.plantId)">
       <v-icon>mdi-sprout-outline</v-icon>
     </v-btn>
-    <v-text-field v-model="plant.friendlyName" class="ml-1" label="Plant" variant="solo" density="compact" disabled/>
-    <v-btn class="px-1 mt-2" color="black" size="small" @click="navigateToPlotDetails(planting.plotId)">
+    <v-text-field
+        v-model="plant.friendlyName"
+        class="ml-1"
+        label="Plant"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled()"/>
+  </div>
+
+  <div class="d-flex">
+    <v-text-field
+        v-model="plantingCopy.leadTimeWeeks"
+        label="Transplant Lead Time (weeks)"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('CREATED')"/>
+    <v-text-field
+        v-model="plantingCopy.sowDate"
+        label="Sow Date"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('STARTED')"/>
+    <v-text-field
+        v-model="plantingCopy.numberSown"
+        label="Number Sown"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('STARTED')"/>
+  </div>
+
+  <div class="d-flex">
+    <v-btn class="px-1 mt-2" color="black" size="small" @click="navigateToPlotDetails(plantingCopy.plotId)">
       <v-icon>mdi-vector-square</v-icon>
     </v-btn>
-    <v-text-field v-model="plotName" class="ml-1" label="Plot" variant="solo" density="compact" disabled/>
+    <v-text-field
+        v-model="plotName"
+        class="ml-1"
+        label="Plot"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('PLANTED')"/>
   </div>
 
   <div class="d-flex">
-    <v-text-field v-model="sowDate" label="Sow Date" variant="solo" density="compact" disabled/>
-    <v-text-field v-model="leadTime" label="Transplant Lead Time" variant="solo" density="compact" disabled/>
-  </div>
-
-  <div class="d-flex">
-    <v-text-field v-model="transplantDate" label="Transplant Date" variant="solo" density="compact" disabled/>
-    <v-text-field v-model="numberOfPlants" label="Number of Plants" variant="solo" density="compact" disabled/>
+    <v-text-field
+        v-model="plantingCopy.transplantDate"
+        label="Planting Date"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('PLANTED')"/>
+    <v-text-field
+        v-model="plantingCopy.numberTransplanted"
+        label="Number Planted"
+        variant="solo"
+        density="compact"
+        :disabled="isFieldEditDisabled('PLANTED')"/>
   </div>
 </template>
 
 <script>
+import plantingUtils from '@/utils/plantings';
+
+const { plantingStatusesList } = plantingUtils;
+
 export default {
   name: "PlantingDetailsTab",
 
@@ -41,6 +122,27 @@ export default {
     planting: Object,
     plant: Object,
     plot: Object,
+  },
+
+  data() {
+    return {
+      enableEdit: false,
+      plantingCopy: {
+        plantingId: this.planting.plantingId,
+        plotId: this.planting.plotId,
+        plantId: this.planting.plantId,
+        plantingYear: this.planting.plantingYear,
+        name: this.planting.name,
+        seedSource: this.planting.seedSource,
+        lotNumber: this.planting.lotNumber,
+        leadTimeWeeks: this.planting.leadTimeWeeks,
+        sowDate: this.planting.sowDate,
+        sowType: this.planting.sowType,
+        numberSown: this.planting.numberSown,
+        transplantDate: this.planting.transplantDate,
+        numberTransplanted: this.planting.numberTransplanted,
+      },
+    };
   },
 
   computed: {
@@ -55,31 +157,16 @@ export default {
     plotName() {
       return this.plot ? this.plot.friendlyName : '---';
     },
-
-    sowDate() {
-      return this.planting.sowDate
-          ? this.planting.sowDate
-          : '---';
-    },
-
-    transplantDate() {
-      return this.planting.transplantDate
-          ? this.planting.transplantDate
-          : '---';
-    },
-
-    numberOfPlants() {
-      return this.planting.numberTransplanted || this.planting.numberSown || '---';
-    },
-
-    leadTime() {
-      return this.planting.leadTimeWeeks
-          ? this.planting.leadTimeWeeks + ' weeks'
-          : '---';
-    },
   },
 
   methods: {
+    isFieldEditDisabled(allowedStatuses = [...plantingStatusesList]) {
+      if (!this.enableEdit) {
+        return true;
+      }
+      return !allowedStatuses.includes(this.planting.currentStatus);
+    },
+
     navigateToPlotDetails(plotId) {
       console.log('plot clicked, do something in the future');
     },
