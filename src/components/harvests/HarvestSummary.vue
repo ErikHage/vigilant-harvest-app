acfa457193a06ef0f4974f9d56246263fef66be7
Erik Hage <ehage4@gmail.com>
2025-05-21 23:45:36 -0400

    use flexbox instead of a table for planting/harvest details layout in garden page

--- src/components/harvests/HarvestSummary.vue
+++ src/components/harvests/HarvestSummary.vue
@@ -1,31 +1,25 @@
 <template>
   <v-card>
-    <v-card-title class="text-center">
-      <h3>{{ hydratedPlot.friendlyName }}</h3>
+    <v-card-title class="d-flex justify-start mb-2">
+      <h4>{{ hydratedPlot.friendlyName }}</h4>
     </v-card-title>
     <v-card-text>
-      <v-table density="compact">
-        <tbody>
-        <tr v-for="planting in hydratedPlot.plantings">
-          <td>
-            <v-chip class="ml-2 mr-2" color="green">
-              {{ planting.numberTransplanted }}<v-icon class="ml-1">mdi-leaf</v-icon>
-            </v-chip>
-          </td>
-          <td class="text-center">
-            <span>{{ planting.plant.friendlyName }}</span>
-          </td>
-          <td class="text-right">
-            <v-chip v-if="planting.harvestQuantity > 0" color="yellow">
-              {{ planting.harvestQuantity }}<v-icon class="ml-1">mdi-basket-fill</v-icon>
-            </v-chip>
-            <v-chip v-else color="grey">
-              <v-icon>mdi-basket</v-icon>
-            </v-chip>
-          </td>
-        </tr>
-        </tbody>
-      </v-table>
+      <template v-for="planting in hydratedPlot.plantings">
+        <div class="d-flex justify-space-between align-center">
+          <v-chip class="ml-2 mr-2" color="green">
+            {{ planting.numberTransplanted }}
+            <v-icon class="ml-1">mdi-leaf</v-icon>
+          </v-chip>
+          <span>{{ planting.plant.friendlyName }}</span>
+          <v-chip v-if="planting.harvestQuantity > 0" color="yellow">
+            {{ planting.harvestQuantity }}
+            <v-icon class="ml-1">mdi-basket-fill</v-icon>
+          </v-chip>
+          <v-chip v-else color="grey">
+            <v-icon>mdi-basket</v-icon>
+          </v-chip>
+        </div>
+      </template>
     </v-card-text>
   </v-card>
 </template>
